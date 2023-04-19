50b28f63a34573720ff87321f96bd714aee6fe56
Makkos Peter <makkos.peter@students.jedlik.eu>
2023-04-19 09:28:04 +0200

    js joci

---
+++ script.js
@@ -0,0 +1,93 @@
+function valt(id){
+
+
+switch (id) {
+    case '1': 
+        document.getElementById('1').style.left = '10%';
+        document.getElementById('2').style.left = '100%';
+        document.getElementById('3').style.left = '100%';
+        document.getElementById('4').style.left = '100%';
+        document.getElementById('5').style.left = '100%';
+        document.getElementById('6').style.left = '100%';
+        document.getElementById('7').style.left = '100%';
+        document.getElementById('8').style.left = '100%';
+        break;
+    
+    case '2': 
+        document.getElementById('1').style.left = '100%';
+        document.getElementById('2').style.left = '10%';
+        document.getElementById('3').style.left = '100%';
+        document.getElementById('4').style.left = '100%';
+        document.getElementById('5').style.left = '100%';
+        document.getElementById('6').style.left = '100%';
+        document.getElementById('7').style.left = '100%';
+        document.getElementById('8').style.left = '100%';
+        break;
+  
+    case '3': 
+        document.getElementById('1').style.left = '0%';
+        document.getElementById('2').style.left = '0%';
+        document.getElementById('3').style.left = '100%';
+        document.getElementById('4').style.left = '0%';
+        document.getElementById('5').style.left = '0%';
+        document.getElementById('6').style.left = '0%';
+        document.getElementById('7').style.left = '0%';
+        document.getElementById('8').style.left = '0%';
+        break;
+    
+    case '4': 
+        document.getElementById('1').style.left = '0%';
+        document.getElementById('2').style.left = '0%';
+        document.getElementById('3').style.left = '0%';
+        document.getElementById('4').style.left = '100%';
+        document.getElementById('5').style.left = '0%';
+        document.getElementById('6').style.left = '0%';
+        document.getElementById('7').style.left = '0%';
+        document.getElementById('8').style.left = '0%';
+        break;
+
+    case '5': 
+        document.getElementById('1').style.left = '0%';
+        document.getElementById('2').style.left = '0%';
+        document.getElementById('3').style.left = '0%';
+        document.getElementById('4').style.left = '0%';
+        document.getElementById('5').style.left = '100%';
+        document.getElementById('6').style.left = '0%';
+        document.getElementById('7').style.left = '0%';
+        document.getElementById('8').style.left = '0%';
+        break;
+
+    case '6': 
+        document.getElementById('1').style.left = '0%';
+        document.getElementById('2').style.left = '0%';
+        document.getElementById('3').style.left = '0%';
+        document.getElementById('4').style.left = '0%';
+        document.getElementById('5').style.left = '0%';
+        document.getElementById('6').style.left = '100%';
+        document.getElementById('7').style.left = '0%';
+        document.getElementById('8').style.left = '0%';
+        break;
+
+    case '7': 
+        document.getElementById('1').style.left = '0%';
+        document.getElementById('2').style.left = '0%';
+        document.getElementById('3').style.left = '0%';
+        document.getElementById('4').style.left = '0%';
+        document.getElementById('5').style.left = '0%';
+        document.getElementById('6').style.left = '0%';
+        document.getElementById('7').style.left = '100%';
+        document.getElementById('8').style.left = '0%';
+         break;
+
+    case '8': 
+        document.getElementById('1').style.left = '0%';
+        document.getElementById('2').style.left = '0%';
+        document.getElementById('3').style.left = '0%';
+        document.getElementById('4').style.left = '0%';
+        document.getElementById('5').style.left = '0%';
+        document.getElementById('6').style.left = '0%';
+        document.getElementById('7').style.left = '0%';
+        document.getElementById('8').style.left = '100%';
+        break;
+}
+}
