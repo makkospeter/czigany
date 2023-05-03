091aa7007b50b4fb55775c944a63e26ef9e60fc1
Makkos Peter <makkos.peter@students.jedlik.eu>
2023-05-03 08:23:51 +0200

    pusholas

--- script.js
+++ script.js
@@ -4,36 +4,36 @@ var containers = Array.from(document.getElementsByClassName("beniDad"));
 containers.forEach((currVal, index, arr) => {
     currVal.style.left = "200%";
 });
-containers[0].style.left = '10%';
-function valt(id){
-console.log(containers);
-console.log(width);
+containers[0].style.left = '0%';
+function valt(id) {
+    console.log(containers);
+    console.log(width);
 
-containers.forEach((currVal, index, arr) => {
-    currVal.style.left = "200%";
-});
-currId = id;
-if(width > 700) {
-    containers[id].style.left = "15%";
-}
-else if(width <= 700) {
-    containers[id].style.left = "-0%";
-}
+    containers.forEach((currVal, index, arr) => {
+        currVal.style.left = "200%";
+    });
+    currId = id;
+    if (width > 700) {
+        containers[id].style.left = "0%";
+    }
+    else if (width <= 700) {
+        containers[id].style.left = "0%";
+    }
 }
 
 window.addEventListener("resize", (event) => {
     width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
     console.log(currId);
-    if(width > 700) {
-        containers[currId].style.left = "15%";
+    if (width > 700) {
+        containers[currId].style.left = "0%";
     }
-    else if(width <= 700) {
-        containers[currId].style.left = "-0%";
+    else if (width <= 700) {
+        containers[currId].style.left = "0%";
     }
 });
 
-document.addEventListener("ready", function(){
-	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
-		$(this).toggleClass('open');
-	});
+document.addEventListener("ready", function () {
+    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function () {
+        $(this).toggleClass('open');
+    });
 });
